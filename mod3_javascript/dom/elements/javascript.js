8eedb7cd1d3d9c32af05d973ad7e7044fc4f0682
montoyaguzman <montoyaguzman7@gmail.com>
2018-12-27 15:28:35 -0600

    ejemplo de objeto element

---
+++ mod3_javascript/dom/elements/javascript.js
@@ -0,0 +1,8 @@
+let element1 = document.getElementById('primero').innerHTML
+let element2 = document.getElementsByClassName('segundo')[0].innerHTML
+let element3 = document.getElementsByTagName('span')[0].innerHTML
+
+console.log('cargue el js...')
+console.log(element1)
+console.log(element2)
+console.log(element3)
